27f78456ea82ca183e3c69d52070913d89881b8b
Haritz Medina <haritzmedina@gmail.com>
2017-01-03 13:48:20 +0100

    Fixed security code changing. Now retrieves new security code if it has changed. Returns no info if the bus stop has no arrivals or there is an error

--- app.js
+++ app.js
@@ -1,28 +1,96 @@
-const querystring = require('querystring');
+var restify = require('restify');
+var builder = require('botbuilder');
 const request = require('request');
 const cheerio = require('cheerio');
-const Telegraf = require('telegraf');
+const querystring = require('querystring');
 
-const app = new Telegraf('303114484:AAH-nTKyTcigJMxB5ugoF1HHAeCJQQywOFI');
+//=========================================================
+// Bot Setup
+//=========================================================
 
-app.command('start', (ctx) => {
-    console.log('start', ctx.from);
-    ctx.reply('Welcome!');
+// Setup Restify Server
+var server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, function () {
+    console.log('%s listening to %s', server.name, server.url);
 });
 
-app.hears('hi', (ctx) => ctx.reply('Hey there!'));
+// Create chat bot
+var connector = new builder.ChatConnector({
+    appId: 'aaf64664-3085-42a5-ae07-617720884399',
+    appPassword: 'mtdotVRj9CyeQdUhKvD78xS'
+});
+var bot = new builder.UniversalBot(connector);
+server.post('/api/messages', connector.listen());
 
-app.hears('dbus*', (ctx)=>{
-    console.log('aa');
+var securityString = '';
+
+//=========================================================
+// Bots Dialogs
+//=========================================================
+
+bot.dialog('/', new builder.IntentDialog()
+    .matches(/^parada/i, '/parada')
+    .onDefault(builder.DialogAction.send("Para saber los horarios de una parada, di 'parada'"))
+);
+
+bot.dialog('/profile', [
+    function (session) {
+        builder.Prompts.text(session, 'Hi! What is your name?');
+    },
+    function (session, results) {
+        session.userData.name = results.response;
+        session.endDialog();
+    }
+]);
+
+bot.dialog('/parada', [
+    function(session){
+        builder.Prompts.text(session, 'Que parada quieres? Ejemplo.:\n\n' +
+            '3141 - Pio XII\n\n' +
+            '2299 - Unibertsitatea Tol.77\n\n' +
+            '3082 - Unibertsitatea T.70 II');
+    },
+    function (session, results) {
+        let parada = results.response;
+        console.log('Asking for parada: '+parada);
+        requestArrivals(parada, (llegadas)=>{
+            if(llegadas.length===0){
+                session.send('No hay informacion o la parada no existe');
+            }
+            else{
+                for(let i=0;i<llegadas.length;i++){
+                    session.send(llegadas[i]);
+                }
+            }
+            session.endDialog();
+        });
+    }
+]);
+
+function requestArrivals(parada, callback){
     request.post('http://www.dbus.eus/wp-admin/admin-ajax.php',{
-        form: querystring.stringify({action: 'calcula_parada', parada: '2299', security: 'd5652d88d4'})
+        form: querystring.stringify({action: 'calcula_parada', parada: parada, security: securityString})
     }, function(error, response, html){
+        if(response.body==='-1'){
+            updateSecurityString(()=>{
+                requestArrivals(parada, callback);
+            });
+        }
         let $ = cheerio.load(html);
         let proximasLlegadas = $('#prox_lle').find('li');
+        let llegadas = [];
         for(let i=0;i<proximasLlegadas.length;i++){
-            ctx.reply(proximasLlegadas[i].firstChild.data);
+            llegadas.push(proximasLlegadas[i].firstChild.data)
         }
+        callback(llegadas);
     });
-});
+}
 
-app.startPolling();
+function updateSecurityString(callback){
+    request.get('http://www.dbus.eus/05-benta-berri/', {}, (error, response, html)=>{
+        let testRE = html.match("security: '(.*)'");
+        securityString = testRE[1];
+        console.log('Security string set: '+securityString);
+        callback();
+    });
+}
